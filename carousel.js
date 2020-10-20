4a02ceb043c80e32490591b02b7cb77b91019bd0
Jacob <jacobc1596@gmail.com>
2020-10-20 12:26:42 +0100

    Added Submission Guidelines

---
+++ carousel.js
@@ -0,0 +1,16 @@
+//Image Carousel
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    breakpoints: [{ 
+        changePoint: 480, slidesToShow: 1, slidesToScroll: 1 
+    }, { 
+        changePoint: 640, slidesToShow: 1, slidesToScroll: 1 
+    }, { 
+        changePoint: 768, slidesToShow: 1, slidesToScroll: 1 
+    } ],
+    autoplay: true,
+    loop: true,
+    infinite:true,
+    autoplaySpeed: 5000
+});
--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//Image Carousel
-bulmaCarousel.attach('#carousel-demo', {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    breakpoints: [{ 
-        changePoint: 480, slidesToShow: 1, slidesToScroll: 1 
-    }, { 
-        changePoint: 640, slidesToShow: 1, slidesToScroll: 1 
-    }, { 
-        changePoint: 768, slidesToShow: 1, slidesToScroll: 1 
-    } ],
-    autoplay: true,
-    loop: true,
-    infinite:true,
-    autoplaySpeed: 5000
-});
-
 //Hamburger Menu
 const navbarMenu = document.querySelector('#nav-links');
 const burgerIcon = document.querySelector('#burger');
